1416f138f6f5be1022469bfbd335b9527e2cbec1
Deepaksinha2022 <117368805+Deepaksinha2022@users.noreply.github.com>
2023-03-14 19:23:21 +0530

    Add files via upload

---
+++ Interview_questions.py
@@ -0,0 +1,95 @@
+# 5 interview question on data types
+
+# Q1      - What are Data Types?
+# Answer  - Data types are any set of values
+#         - example - we have data types like int(2,54 etc), float(2.3,0.9 etc),complex(1+5j),list([1,48,-1])
+#         - strings("a","hello"),set({4,56,"abc"}),dictionary({"a":2,"z":56}),tuple((5,8,9,4,23)) 
+
+
+# Q2      - Name some of the main data types in Python 
+# Answer  - Some of main data types in python are int,float,string,list,tuple,dictionary,set,complex,None Type,Bool type
+
+
+# Q3      - Why are these data types known as Python’s core data types?
+# Answer  - These data types are core because they serve as basis to deal with different types of data
+#         - They are used to create objects,manupulate,write function among others
+
+# Q4      - What does immutable mean and what three types of Python core data types are considered immutable?
+# Answer  - Immutable means that it cannot be modified after its creation 
+#         - Int,float,string,tuple are immutable datatypes
+
+# Q5      - What does sequence mean and which three types of data fall into this category?
+# Answer  - A sequence data type is a collection of objects ordered by a specific position. In Python, Strings, lists, and tuples are the data types based on sequences
+
+
+# 5 Questions on Operators
+
+# Q1      - What are operators ?
+# Answer  - Oparators are special symbols that are used to perform different operations
+
+# Q2      - What are different types (with examples) of Operators in python ?
+# Answer  - 1. Arithmetic Operator  -  +,-,*,/
+#           2. Assignment Operator  -  =,+=,-=,/=
+#           3. Comparison Operator  -  >,<,>=,<=
+#           4. Logical Operator     -  or,and,not
+#           5. Identity Operator    - is, isnot
+#           6. Membership Operator  - in,not in
+
+# Q3      - What is operator overloading ?
+# Answer  - This feature  allows the same operator to have different meaning according to the context.
+#         - To overload the + operator, we can implement __add__() function in a class.
+
+
+# Q4      - What is the difference between /, // ,% operators?
+# Answer  - " / "  - it  gives the result in decimal and doesnot ignore the value after decimal
+#         - "// "  - It ignore the value after decimal and returns tha floor value
+#         - " % "  - It will return the remainder value after division
+
+# Q5      - What are logical operators ?
+# Answer  - and, or and not are the examples of logical operators
+
+
+# 5 Questions on Conditional Statements
+
+# Q1      - 1. What in “True” in Python?
+# Answer  - Any expression that does not evaluate to 0
+
+# Q2      - What are the three main conditional statements in Python?
+# Answer  - if, elif, and else
+
+# Q3      - Illustrate a basic if, elif, else structure.
+# Answer  - if <condition>:
+#         - . . .
+#         - elif<another condition>:
+#         - . . .
+#         - else:
+#         - . . .
+
+# Q4      - What does elif mean?
+# Answer  - it means else if. It is used after an if statement, to do another comparison.
+
+# Q5      - How are if, elif, and else blocks defined?
+# Answer  - All blocks in Python are defined by indenting. All lines of a particular code block must have the same level of indenting.
+
+
+
+
+# 3 Questions on looping Statements
+
+# Q1      - Which statement is used inside a loop to skip the rest of the code for the current iteration, so execution can continue with the next iteration?
+# Answer  - continue
+
+# Q2      - Which loop in Python is a counter-controlled loop?
+# Answer  - In Python, for loop is a counter-controlled loop. The counter determines how many times the for loop executes
+
+# Q3      - What does the break keyword do?
+# Answer  -  Exits the loop immediately
+
+# 2 Questions on Functions
+
+# Q1      - What are the two types of functions in Python?
+# Answer  - There are two types of functions in Python: built-in functions and user-defined functions.
+
+# Q2      - Do Python functions have return values? If yes, then how many can they have?
+# Answer  - Yes,They can have a single return value, or they can have multiple return values.
+
